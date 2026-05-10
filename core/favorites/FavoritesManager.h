0467ecad2f7cb34e781e3f360895716d612e66fc
AbelChocca <abelchocca1010@gmail.com>
2026-05-10 00:17:04 -0500

    Add FavoritesManager class for managing user favorites functionality

---
+++ core/favorites/FavoritesManager.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "favorites/Favorite.h"
+
+class Persona;
+class Multimedia;
+class LogHandler;
+
+class FavoriteManager {
+
+private:
+
+    std::vector<Favorite> favorites;
+
+    std::string filename;
+
+public:
+
+    FavoriteManager(
+        const std::string& filename
+    );
+
+    void load();
+
+    void save();
+
+    bool isFavorite(
+        int userId,
+        int mediaId
+    ) const;
+
+    void addFavorite(
+        Persona* user,
+        Multimedia* media,
+        LogHandler& logger
+    );
+
+    std::vector<int> getFavoritesByUser(
+        int userId
+    ) const;
+};
