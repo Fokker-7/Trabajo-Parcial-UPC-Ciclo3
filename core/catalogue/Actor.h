89c5ad42e0a98b75c3059de827e62ad22c529583
Fokker-7 <juantolentino7102004@gmail.com>
2026-05-10 17:52:36 -0500

    refactor: mejorar la gestión de contactos en las clases Actor y Director

--- core/catalogue/Actor.h
+++ core/catalogue/Actor.h
@@ -1,25 +1,22 @@
 #pragma once
-#include <iostream>
-#include <string>
-
-using namespace std;
+#include "Contacto.h"
 
 class Actor
 {
 private:
     string nombre;
     string papelPrincipal;
+    Contacto contacto;
 public:
-    Actor(string nom, string papel){
-        nombre=nom;
-        papelPrincipal=papel;
-    }
+    Actor(string nom, string papel, string p="", string e=""): nombre(nom), papelPrincipal(papel), contacto(p, e){}
     ~Actor(){}
 
     string getNombre(){return nombre;}
 
     void printInfo(){
         cout<<"[Actor] "<< nombre<<" | Papel Destacado: "<<papelPrincipal;
+        cout << "           Contacto -> Email: " << contacto.getEmail() 
+         << " | Tel: " << contacto.getPhone() << endl;
     }
 };
 
--- core/catalogue/Director.h
+++ core/catalogue/Director.h
@@ -1,24 +1,20 @@
 #pragma once
-#include <iostream>
-#include <string>
-
-using namespace std;
-
+#include "Contacto.h"
 class Director
 {
 private:
     string nombre;
     int pelDirigidas;
+    Contacto contacto;
 public:
-    Director(string nom, int pel){
-        nombre=nom;
-        pelDirigidas=pel;
-    }
-    ~Director();
+    Director(string nom, int pel, string p="", string e=""): nombre(nom), pelDirigidas(pel), contacto(p, e){}
+    ~Director(){}
 
     string getNombre(){return nombre;}
     void printInfo(){
         cout<<"[Director] "<<nombre<< " | Obras dirigidas: "<< pelDirigidas<<endl;
+        cout << "           Contacto -> Email: " << contacto.getEmail() 
+         << " | Tel: " << contacto.getPhone() << endl;
     }
 };
 
