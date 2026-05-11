55f3ba8faead3d8e644ae536d837fba637538471
AbelChocca <abelchocca1010@gmail.com>
2026-05-10 21:19:59 -0500

    refactor: simplify heapify function for improved readability and maintainability

--- core/algorithms/HeapSort.h
+++ core/algorithms/HeapSort.h
@@ -1,37 +1,33 @@
 #pragma once
 
 template <typename T, typename Compare>
-void heapify(T arr[], int n, int i, Compare comp){
-    int bigger = i;
-    int left = 2*i+1;
-    int right = 2*i+2;
+void heapify(T arr[], int n, int i, Compare comp) {
 
-    if (left<n && comp(arr[left], arr[bigger]))
-    {
-        bigger=left;
-    }
+    int best = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
 
-    if (right<n && comp(arr[right], arr[bigger]))
-    {
-        bigger=right;
-    }
-    
-    if (bigger!=i)
-    {
-        T temp = arr[i];
-        arr[i]=arr[bigger];
-        arr[bigger]= temp;
+    if (left < n && comp(arr[left], arr[best]))
+        best = left;
 
-        heapify(arr, n, bigger, comp);
+    if (right < n && comp(arr[right], arr[best]))
+        best = right;
+
+    if (best != i) {
+        std::swap(arr[i], arr[best]);
+        heapify(arr, n, best, comp);
     }
 }
 
 template <typename T, typename Compare>
-void heapSort(T arr[], int n, Compare comp){
-    for (int i = n/2 -1; i>= 0; i--){
-        T temp=arr[0];
-        arr[0]= arr[i];
-        arr[i]=temp;
-        heapify (arr, i, 0, comp);
+void heapSort(T arr[], int n, Compare comp) {
+
+    for (int i = n/2 - 1; i >= 0; i--) {
+        heapify(arr, n, i, comp);
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+        std::swap(arr[0], arr[i]);
+        heapify(arr, i, 0, comp);
     }
 }
